484547af5a78fd807dd9aebd5ba5fd93782edfc7
Yvonne Flory <yvflory@gmail.com>
2015-01-22 13:10:04 +0000

    This is the first draft of the assignment. If there is no other draft, please consider with my submission.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix object that can be called by other
+## functions using x$invertMatrix (which inverts the matrix) or
+## x$getMatrix (which returns the matrix).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    invertMatrix <- function(y) {
+        x <<- solve(y) ## global variable contains the inverted matrix
+    }
+    getMatrix <- function() x ## returns the matrix when called
 }
 
 
@@ -12,4 +18,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getMatrix()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    x$invertMatrix(m)
+    m
 }
